b4e27ba512f344c745762e5aaa08af0b86feef63
Xh Owais <87980770+XhOwais@users.noreply.github.com>
2023-08-28 08:48:46 +0500

    Home Responsive

--- src/home.jsx
+++ src/home.jsx
@@ -31,18 +31,18 @@ export default function Home() {
                 <div>
                     {cars.map((car) => (
                         <Link to={`/car/${car._id}`} key={car._id} >
-                            <div className=' w-full h-200 flex items-center justify-between  px-120 mt-32'>
+                            <div className=' w-full h-200 flex items-center justify-between  px-120 mobile:px-60 smallmobile:px-30 mt-32'>
                             <div className=' flex'>
                             <div >
                                 <img className=' w-200 bg-heading h-150 rounded-xl' src={car.photo} alt={`${car.make} ${car.model}`} />
                             </div>
                             <div className=' ml-16'>
-                                <h2 className=' font-heading text-24 pb-16'>{car.makemodel}</h2>
+                                <h2 className=' font-heading text-24 pb-16  mobile:text-16'>{car.makemodel}</h2>
                                 <p className=' font-body'>Year: {car.year}</p>
                                 <p>Registered In: {car.registeredin}</p>
                             </div>
                             </div>
-                            <button className=' px-30 py-30 bg-heading text-white rounded-full hover:bg-first font-heading'>Dicover</button>
+                            <button className=' px-30 py-30 mobile:px-20 mobile:py-20 bg-heading text-white rounded-full hover:bg-first font-heading'>Dicover</button>
                             </div>
                         </Link>
                     ))}
