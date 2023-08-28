be64924c5a5a56dc9dc99b8a3196710a9627f985
Xh Owais <87980770+XhOwais@users.noreply.github.com>
2023-08-28 08:30:46 +0500

    Navbar edited

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,24 +2,8 @@ import Logo from '../assets/logoBlack.png';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  const navbarStyle = {
-    width: "98%",
-    margin: "auto",
-    height: "4em",
-    display: "grid",
-    justifyContent: "center",
-    alignItems: "center",
-    background: "rgba(255, 255, 255, 0.16)",
-    borderRadius: "16px",
-    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-    backdropFilter: "blur(5px)",
-    webkitBackdropFilter: "blur(5px)",
-    border: "1px solid rgba(255, 255, 255, 0.15)"
-  };
 
-  const logoStyle = {
-    width: '200px',
-  };
+  
 
   return (
     <>
@@ -29,9 +13,11 @@ export default function Navbar() {
             <li className=''><a href=""><img className=' w-160' src={Logo} alt="" srcset="" /></a></li>
           </ul>
         </div>
-        <Link to="/login" className="custom-link">
+        <div className=' absolute right-20 cursor-pointer'>
+        <Link to="/login" className=" font-heading ">
           Sign Out
         </Link>
+        </div>
       </div>
     </>
   )
